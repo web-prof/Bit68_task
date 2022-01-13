bf01975d2363c6928fcdc23f44c0d14b708051bb
Gogo ishak <ishakg29@gmail.com>
2022-01-13 22:47:18 +0200

    docker-test-mysqlinstallation

--- project/settings.py
+++ project/settings.py
@@ -98,24 +98,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mydb',
-#         'USER': 'root',
-#         'PASSWORD': 'admin',
-#         'HOST':'localhost',
-#         'PORT':'3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Bit68',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
+    }
+}
 
 
 # Password validation
--- tests/test_ex1.py
+++ tests/test_ex1.py
@@ -1,10 +1,34 @@
 import pytest
 from django.contrib.auth.models import User
+from products.models import Products
+from rest_framework.test import APIClient, APITestCase
+from mixer.backend.django import mixer
+from django.test import TestCase
 
 
-@pytest.mark.django_db
-def test_user_create():
-    User.objects.create_user('test','test@test.com','test')
-    assert User.objects.count() == 1
+pytest_mark = pytest.mark.django_db
 
 
+class Test_Products_APIViews(TestCase):
+    def setup(self):
+        self.client = APIClient()
+
+    def test_product_list_work(self):
+        # create_product
+        product = mixer.blend(Products, prd_name='labtop')
+        # call the url
+        response = self.client.get('/products/')
+        # assertion
+        assert response.json != None
+        assert response.status_code == 200
+        assert product.prd_name == 'labtop'
+
+
+# class Test_authentication(APITestCase):
+#     def authenticate(self):
+#         self.client.post(reverse("accounts:register"),{"username":"username","email":"email.gmail.com","password":"password"})
+#         response=self.client.post('/api/token/',{"username":"username","password":"password"})
+#         self.clien.credentials(HTTP_AUTHORIZATION=f"Bearer {response.data['token']}")
+#         def endpoint_need_auth_to_be_Tested(self):
+#             #test the end point here
+#             pass
